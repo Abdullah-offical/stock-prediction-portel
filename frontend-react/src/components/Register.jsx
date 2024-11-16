82b789ce2a13218cc3856039f7b0326aeff0b602
abdullah <apple@apples-MacBook-Air.local>
2024-11-16 19:38:53 +0500

    get user data to setState

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, {useState}  from 'react'
 
 const Register = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleRegistration = (e) => {
+        e.preventDefault()
+        // Perform registration logic here
+        const userData = {
+            username,
+            email,
+            password
+        }
+        console.log(userData)
+        
+    }
   return (
     <>
         <div className='container'>
         <div className="row justify-content-center">
             <div className="col-md-6 bg-light-dark p-5 rounded">
                 <h3 className='text-light text-center mb-4'>Create an Account</h3>
-                <form >
+                <form onSubmit={handleRegistration}>
                   <div className='mb-3'>
-                    <input type="text" className='form-control' placeholder='Username' />
+                    <input type="text" className='form-control' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
                   </div>
                     <div className='mb-3'>
-                      <input type="email" className='form-control' placeholder='Email address' />
+                      <input type="email" className='form-control' placeholder='Email address' value={email} onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className='mb-3'>
-                    <input type="password" className='form-control ' placeholder='Set password' />
+                    <input type="password" className='form-control ' placeholder='Set password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>    
                       <button type='submit' className='btn btn-info d-block mx-auto'>Register</button>
                 </form>
